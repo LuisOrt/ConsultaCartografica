abd67c118a412b2e3b3faa24b305fa3148d22492
Luis Ortega <luis_que7@hotmail.com>
2019-11-01 09:57:29 -0600

    Ya podemos leer cvs y cargar informacion en ram (listas) para busqueda rapida

--- ConsultaCartografica/Form1.Designer.cs
+++ ConsultaCartografica/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Buscar);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(11, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(321, 239);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(20, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "☼";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(344, 287);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Consulta Cartografica INE";
+            this.Load += new System.EventHandler(this.Inicio);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ConsultaCartografica/Form1.cs
+++ ConsultaCartografica/Form1.cs
@@ -1,19 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Windows.Forms;
 
-namespace ConsultaCartografica
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace ConsultaCartografica{
+
+    public partial class Form1 : Form{
+        List<int> DistritoF = new List<int>();
+        List<int> DistritoL = new List<int>();
+        List<string> Municipio = new List<string>();
+        List<int> ClaveLocalidad = new List<int>();
+        List<string> localidades = new List<string>();
+        List<int> tipo = new List<int>();
+        List<int> Seccion = new List<int>();
+
+        public Form1(){
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e){
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()){
+                openFileDialog.Filter = "txt files (*.csv)|*.csv";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK){
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+                    cargador(filePath);
+                }
+            }
+        }
+
+        private void cargador(String filePath){          
+            var reader = new StreamReader(File.OpenRead(filePath));
+            string tit=reader.ReadLine();
+            while (!reader.EndOfStream){
+                try
+                {
+                    var line = reader.ReadLine();
+                    string[] words = line.Split('|');
+                    DistritoF.Add(Int32.Parse(words[1]));
+                    DistritoL.Add(Int32.Parse(words[2]));
+                    Municipio.Add(words[3]);
+                    ClaveLocalidad.Add(Int32.Parse(words[4]));
+                    localidades.Add(words[5]);
+                    tipo.Add(words[6] == "U" ? 1 : 2);
+                    Seccion.Add(Int32.Parse(words[7]));
+                }
+                catch (Exception e) {
+                    MessageBox.Show("Asegurese de contener los campos: Entidad|Distrito F|Distrito L|Municipio|Clave Localidad|Nombre Localidad|Categoría|Sección \r" + reader.ReadLine(),
+                   "Campos",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Exclamation,
+                   MessageBoxDefaultButton.Button1);
+                }
+            }
+            richTextBox1.AppendText(localidades.Count + " Localidades cargadas\r");
+            DateTime creation = File.GetCreationTime(filePath);
+            richTextBox1.AppendText( "Fecha de corte: " + creation);
+        }
+
+        private void Inicio(object sender, EventArgs e) {
+            try{
+                string[] docs = Directory.GetFiles(@"C:\AC-10-R", "*.csv");
+                string ruta = docs[0];
+                cargador(ruta);
+            }
+            catch(System.IO.DirectoryNotFoundException f) {
+                MessageBox.Show("Coloce el AC-10-R en la carpeta C:\\AC-10-R");
+                Directory.CreateDirectory("C:\\AC-10-R");
+                System.Diagnostics.Process.Start("C:\\AC-10-R");
+                Close();
+            }
+            catch(System.IndexOutOfRangeException o){
+                MessageBox.Show("Coloce el AC-10-R en la carpeta C:\\AC-10-R");
+                System.Diagnostics.Process.Start("C:\\AC-10-R");
+                Close();
+            }
+            
+        }
+
+        private void Buscar(object sender, EventArgs e){
+            string busc = textBox1.Text;
+            string[] part = busc.Split(' ');
+            switch (part[0]){
+                case "sec":
+                    int sec = Int32.Parse(part[1]);
+                    richTextBox1.Text = "Seccion "+sec+"\n Contiene las localidades \n";
+                    for(int i=0; i < Seccion.Count; i++){
+                        if (Seccion[i] == sec) {
+                            richTextBox1.AppendText(localidades[i] + "\n"
+                                + ClaveLocalidad[i]
+                                + (tipo[i] == 1 ? "Urbana" : "Rural")
+                                + "\n");
+                        }
+                    }
+                    break;
+
+                case "tot":
+                    break;
+
+                case "loc":
+                    break;
+
+                default:
+
+                    break;
+            }
+                
+
         }
     }
 }
